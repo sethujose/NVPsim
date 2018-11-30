0a7133edd54f20dc9bf73c86a4d733a667054979
Sethu Jose <sethujose@gmail.com>
2018-11-30 15:01:59 -0500

    bug fix for retention state, added firmware trace for bare gem5 and nvpsim execution

--- profile/nvp_sim/sine_wave/sine_wave.m
+++ profile/nvp_sim/sine_wave/sine_wave.m
@@ -1,4 +1,4 @@
-fs = 1000;
+fs = 10000;
 f = 1;
 t = 0 : 1/fs : 10 - 1/fs;
 x = sin(pi*f/10*t);
@@ -6,4 +6,4 @@ y(1:1000) = 0;
 z = [x y];
 plot(z);
 z = z.';
-save high_res_sine z;
+save high_res_sine_10K z;
